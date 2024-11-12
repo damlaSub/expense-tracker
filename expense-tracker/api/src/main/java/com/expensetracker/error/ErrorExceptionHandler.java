da94a1191f44e39994453c2ceab42087615938a0
damlaSub <mme.dalloway@gmail.com>
2024-11-12 21:58:40 +0100

    bad credentials exception

--- expense-tracker/api/src/main/java/com/expensetracker/error/ErrorExceptionHandler.java
+++ expense-tracker/api/src/main/java/com/expensetracker/error/ErrorExceptionHandler.java
@@ -3,11 +3,22 @@ package com.expensetracker.error;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
 public class ErrorExceptionHandler extends ResponseEntityExceptionHandler{
+	
+	 @ExceptionHandler(BadCredentialsException.class)
+	    protected ResponseEntity<ErrorMessage> handleBadCredentialsException(
+		    BadCredentialsException ex) {
+		ErrorMessage errorMessage = new ErrorMessage(
+			HttpStatus.UNAUTHORIZED.value(),
+			"Unauthorized", ex.getMessage());
+		return new ResponseEntity<>(errorMessage,
+			HttpStatus.UNAUTHORIZED);
+	    }
 
     @ExceptionHandler(AccountNotFoundException.class)
     protected ResponseEntity<ErrorMessage> handleAccountNotFoundException(
