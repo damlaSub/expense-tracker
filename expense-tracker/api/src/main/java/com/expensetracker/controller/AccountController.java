f754c58450380fb153b08830628aa6e41a884f0b
damlaSub <mme.dalloway@gmail.com>
2024-11-14 18:26:21 +0100

    metadata for app props

--- expense-tracker/api/src/main/java/com/expensetracker/controller/AccountController.java
+++ expense-tracker/api/src/main/java/com/expensetracker/controller/AccountController.java
@@ -1,6 +1,5 @@
 package com.expensetracker.controller;
 
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,7 +24,6 @@ public class AccountController {
 	    public AccountController(AccountService service) {
 		this.service = service;
 	    }
-
 	    @PostMapping("/sign-up")
 	    @ResponseStatus(HttpStatus.NO_CONTENT)
 	    public void signUp(
