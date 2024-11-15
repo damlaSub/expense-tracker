27c518cd74db0bf2f57b86c755a944e6906b9123
damlaSub <mme.dalloway@gmail.com>
2024-11-15 19:20:52 +0100

    bootstrap icons

--- expense-tracker/ui/src/app/dashboard/dashboard.component.ts
+++ expense-tracker/ui/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LocalizationService } from '../services/localization.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,6 +9,16 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  constructor(private router: Router, private localizationService: LocalizationService) {}
+
+  ngOnInit() {
+    this.localizationService.loadLanguage('en'); 
+  }
+
+  $t(key: string): string {
+    return this.localizationService.translate(key);
+  }
+
 
 }
