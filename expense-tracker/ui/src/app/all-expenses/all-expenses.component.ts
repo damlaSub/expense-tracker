19e5055462f12841a599bd77d7cf2704ce91fe93
damlaSub <mme.dalloway@gmail.com>
2024-11-21 14:58:00 +0100

    scroll getAllExpensesByDate

--- expense-tracker/ui/src/app/all-expenses/all-expenses.component.ts
+++ expense-tracker/ui/src/app/all-expenses/all-expenses.component.ts
@@ -1,21 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LocalizationService } from '../services/localization.service';
 import { HttpService } from '../services/http.service';
 import { CommonModule } from '@angular/common';
 
 import { DailyExpensesListComponent } from '../daily-expenses-list/daily-expenses-list.component';
+import { InfiniteScrollDirective } from 'ngx-infinite-scroll';
 
 @Component({
   selector: 'app-all-expenses',
   standalone: true,
-  imports: [CommonModule, DailyExpensesListComponent ],
+  imports: [CommonModule, DailyExpensesListComponent, InfiniteScrollDirective ],
   templateUrl: './all-expenses.component.html',
   styleUrl: './all-expenses.component.scss'
 })
 export class AllExpensesComponent  implements OnInit {
   constructor(private router: Router, private localizationService: LocalizationService, private httpService: HttpService) {}
   expensesReports: any[] = [];
+  page: number = 0; 
+  pageSize: number = 10; 
+  isMoreItem: boolean = true;
 
   // Mapping categories to icons
   categoryIcons: { [key: string]: string } = {
@@ -35,16 +39,21 @@ export class AllExpensesComponent  implements OnInit {
   };
 
   ngOnInit() {
-   // this.localizationService.loadLanguage('en'); 
-    this.fetchAllExpenses();
+    this.localizationService.loadLanguage('en'); 
+    this.fetchAllExpenses(this.page);
   }
 
   $t(key: string): string {
     return this.localizationService.translate(key);
   }
 
-  fetchAllExpenses(): void {
-    const endpoint = 'expenses/by-date';
+  getIconForCategory(category: string): string {
+    return this.categoryIcons[category] || 'bi-question-circle'; 
+  }
+
+
+  fetchAllExpenses(page: number): void {
+    const endpoint = `expenses/by-date?page=${page}&size=${this.pageSize}`;
     this.httpService.get(endpoint).subscribe(
       (data: any) => this.processAllExpensesData(data),
       (error) => console.error('Error fetching all expenses:', error)
@@ -52,10 +61,28 @@ export class AllExpensesComponent  implements OnInit {
   }
 
   processAllExpensesData(response: any): void {
-    console.log('Response Data:', response); 
-    this.expensesReports = response;
+    console.log('Response Data:', response);
+    console.log('Response Length:', response.length);
+    console.log('Page Size:', this.pageSize);
+    console.log('this.isMoreItem:', this.isMoreItem);
+
+    const expenseLengthInResponse = response.reduce((total: any, item: { expenses: string | any[]; }) => total + (item.expenses?.length || 0), 0);
+    if (expenseLengthInResponse === 0 || expenseLengthInResponse < this.pageSize) {
+      console.log('No more expenses to fetch.');
+      this.isMoreItem = false;
+    }
+
+    this.expensesReports = [...this.expensesReports, ...response];
   }
-  getIconForCategory(category: string): string {
-    return this.categoryIcons[category] || 'bi-question-circle'; // Default icon for unknown categories
+
+  onScroll(): void {
+    if (this.isMoreItem) {
+      this.page++;
+      console.log('Loading more data: Page', this.page);
+      this.fetchAllExpenses(this.page);
+    } else {
+      console.log('No more items to load');
+    }
   }
+
 }
