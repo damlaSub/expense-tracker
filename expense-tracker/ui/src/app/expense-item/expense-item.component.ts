b92a6ae705cc4555c8fb329e17f949f747b076f5
damlaSub <mme.dalloway@gmail.com>
2024-12-05 21:23:36 +0100

    delete expense

--- expense-tracker/ui/src/app/expense-item/expense-item.component.ts
+++ expense-tracker/ui/src/app/expense-item/expense-item.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LocalizationService } from '../services/localization.service';
+import { HttpService } from '../services/http.service';
+
 
 @Component({
   selector: 'app-expense-item',
@@ -14,18 +16,31 @@ export class ExpenseItemComponent implements OnInit {
   @Input() expense: { category: string; description?: string; amount: number } = { category: '', amount: 0 };
   @Input() formattedDate: string = '';
   @Input() dailyTotal: number = 0;
-  @Input() expenses: Array<{ category: string; description?: string; amount: number }> = [];
+  @Input() expenses: Array<{ id: number, category: string; description?: string; amount: number }> = [];
   @Input() showDescription: boolean = true;
   @Input() getIconForCategory: (category: string) => string = () => 'bi-question-circle';
   @Input() showButtons: boolean = false;
 
-  constructor( private localizationService: LocalizationService) { }
+  constructor( private localizationService: LocalizationService, private httpService: HttpService ) { }
   ngOnInit() {
     this.localizationService.loadLanguage('en'); 
   }
 
   $t(key: string): string {
     return this.localizationService.translate(key);
+  }
+
+  onDelete(id: number){
+    const endpoint = `expenses/${id}/delete`;
+    this.httpService.delete(endpoint).subscribe({
+      next: (response) => {
+        console.log('Expense deleted successfully', response);
+        this.expenses = this.expenses.filter(expense => expense.id !== id);
+      },
+      error: (error) => {
+        console.error('Error occurred:', error);
+      },
+    });
   }
 
   // Mapping categories to icons
